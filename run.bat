697bfda7617ce6af689e8ce8854e6cabf79ef217
caligiure <caligiure74@gmail.com>
2026-05-31 19:52:47 +0200

    quantum_kernel_cluster_sampling_gpu.py

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Running Quantum Kernel Cluster Sampling on WSL...
+
+wsl -d Ubuntu bash -c "cd /mnt/d/MyProjects/quantum_computing_project && bash run.sh"
+
+echo.
+pause
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /mnt/d/MyProjects/quantum_computing_project
+
+# Activate the virtual environment
+source ~/.venvs/quantum_env/bin/activate
+
+# Run the Python script
+python3 quantum_kernel_cluster_sampling_gpu.py
